d0c944b83f6c7e5780c94d11699734cf463a43f0
yew1120 <89204890+yew1120@users.noreply.github.com>
2023-01-06 14:12:34 +0800

    Add files via upload

---
+++ salesTable.java
@@ -0,0 +1,470 @@
+package intern.app.intern;
+
+/**
+ *
+ * @author chook
+ */
+
+public class salesTable {
+
+    String dimension, dimension2_, dimension3_, dimension4_, dimension5_;
+
+    String address, city, countryRegionID, createdDateTime1;
+
+    String custAccount, custGroup, dataAreaID1, defaultDimension;
+
+    String deliveryPostalAddress, location, mbp_contactPerson1, mbp_contactPerson2;
+
+    String mbp_internalSono, mbp_phone1, mbp_phone2, purchOrderFormNum;
+
+    String salesGroup, salesID, salesName;
+
+    String salesType;
+
+    String state, street, zipcode, dataAreaID;
+
+    String partition, recID, dataAreaID2, partition2;
+
+    String configID, deliveryDate, inventBatchID, inventdimid;
+
+    String inventLocationID, itemID, orderAccount, origSalesID, packingSlipID;
+
+    String qty;
+
+    String salesID1, salesUnit, transRecID;
+
+    String valueMST;
+
+
+
+
+    public String getDimension() {
+        return dimension;
+    }
+
+    public void setDimension(String dimension) {
+        this.dimension = dimension;
+    }
+
+    public String getDimension2_() {
+        return dimension2_;
+    }
+
+    public void setDimension2_(String dimension2_) {
+        this.dimension2_ = dimension2_;
+    }
+
+    public String getDimension3_() {
+        return dimension3_;
+    }
+
+    public void setDimension3_(String dimension3_) {
+        this.dimension3_ = dimension3_;
+    }
+
+    public String getDimension4_() {
+        return dimension4_;
+    }
+
+    public void setDimension4_(String dimension4_) {
+        this.dimension4_ = dimension4_;
+    }
+
+    public String getDimension5_() {
+        return dimension5_;
+    }
+
+    public void setDimension5_(String dimension5_) {
+        this.dimension5_ = dimension5_;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountryRegionID() {
+        return countryRegionID;
+    }
+
+    public void setCountryRegionID(String countryRegionID) {
+        this.countryRegionID = countryRegionID;
+    }
+
+    public String getCreatedDateTime1() {
+        return createdDateTime1;
+    }
+
+    public void setCreatedDateTime1(String createdDateTime1) {
+        this.createdDateTime1 = createdDateTime1;
+    }
+
+    public String getCustAccount() {
+        return custAccount;
+    }
+
+    public void setCustAccount(String custAccount) {
+        this.custAccount = custAccount;
+    }
+
+    public String getCustGroup() {
+        return custGroup;
+    }
+
+    public void setCustGroup(String custGroup) {
+        this.custGroup = custGroup;
+    }
+
+    public String getDataAreaID1() {
+        return dataAreaID1;
+    }
+
+    public void setDataAreaID1(String dataAreaID1) {
+        this.dataAreaID1 = dataAreaID1;
+    }
+
+    public String getDefaultDimension() {
+        return defaultDimension;
+    }
+
+    public void setDefaultDimension(String defaultDimension) {
+        this.defaultDimension = defaultDimension;
+    }
+
+    public String getDeliveryPostalAddress() {
+        return deliveryPostalAddress;
+    }
+
+    public void setDeliveryPostalAddress(String deliveryPostalAddress) {
+        this.deliveryPostalAddress = deliveryPostalAddress;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getMbp_contactPerson1() {
+        return mbp_contactPerson1;
+    }
+
+    public void setMbp_contactPerson1(String mbp_contactPerson1) {
+        this.mbp_contactPerson1 = mbp_contactPerson1;
+    }
+
+    public String getMbp_contactPerson2() {
+        return mbp_contactPerson2;
+    }
+
+    public void setMbp_contactPerson2(String mbp_contactPerson2) {
+        this.mbp_contactPerson2 = mbp_contactPerson2;
+    }
+
+    public String getMbp_internalSono() {
+        return mbp_internalSono;
+    }
+
+    public void setMbp_internalSono(String mbp_internalSono) {
+        this.mbp_internalSono = mbp_internalSono;
+    }
+
+    public String getMbp_phone1() {
+        return mbp_phone1;
+    }
+
+    public void setMbp_phone1(String mbp_phone1) {
+        this.mbp_phone1 = mbp_phone1;
+    }
+
+    public String getMbp_phone2() {
+        return mbp_phone2;
+    }
+
+    public void setMbp_phone2(String mbp_phone2) {
+        this.mbp_phone2 = mbp_phone2;
+    }
+
+    public String getPurchOrderFormNum() {
+        return purchOrderFormNum;
+    }
+
+    public void setPurchOrderFormNum(String purchOrderFormNum) {
+        this.purchOrderFormNum = purchOrderFormNum;
+    }
+
+    public String getSalesGroup() {
+        return salesGroup;
+    }
+
+    public void setSalesGroup(String salesGroup) {
+        this.salesGroup = salesGroup;
+    }
+
+    public String getSalesID() {
+        return salesID;
+    }
+
+    public void setSalesID(String salesID) {
+        this.salesID = salesID;
+    }
+
+    public String getSalesName() {
+        return salesName;
+    }
+
+    public void setSalesName(String salesName) {
+        this.salesName = salesName;
+    }
+
+    public String getSalesType() {
+        return salesType;
+    }
+
+    public void setSalesType(String salesType) {
+        this.salesType = salesType;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getDataAreaID() {
+        return dataAreaID;
+    }
+
+    public void setDataAreaID(String dataAreaID) {
+        this.dataAreaID = dataAreaID;
+    }
+
+    public String getPartition() {
+        return partition;
+    }
+
+    public void setPartition(String partition) {
+        this.partition = partition;
+    }
+
+    public String getRecID() {
+        return recID;
+    }
+
+    public void setRecID(String recID) {
+        this.recID = recID;
+    }
+
+    public String getDataAreaID2() {
+        return dataAreaID2;
+    }
+
+    public void setDataAreaID2(String dataAreaID2) {
+        this.dataAreaID2 = dataAreaID2;
+    }
+
+    public String getPartition2() {
+        return partition2;
+    }
+
+    public void setPartition2(String partition2) {
+        this.partition2 = partition2;
+    }
+
+    public String getConfigID() {
+        return configID;
+    }
+
+    public void setConfigID(String configID) {
+        this.configID = configID;
+    }
+
+    public String getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(String deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getInventBatchID() {
+        return inventBatchID;
+    }
+
+    public void setInventBatchID(String inventBatchID) {
+        this.inventBatchID = inventBatchID;
+    }
+
+    public String getInventdimid() {
+        return inventdimid;
+    }
+
+    public void setInventdimid(String inventdimid) {
+        this.inventdimid = inventdimid;
+    }
+
+    public String getInventLocationID() {
+        return inventLocationID;
+    }
+
+    public void setInventLocationID(String inventLocationID) {
+        this.inventLocationID = inventLocationID;
+    }
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getOrderAccount() {
+        return orderAccount;
+    }
+
+    public void setOrderAccount(String orderAccount) {
+        this.orderAccount = orderAccount;
+    }
+
+    public String getOrigSalesID() {
+        return origSalesID;
+    }
+
+    public void setOrigSalesID(String origSalesID) {
+        this.origSalesID = origSalesID;
+    }
+
+    public String getPackingSlipID() {
+        return packingSlipID;
+    }
+
+    public void setPackingSlipID(String packingSlipID) {
+        this.packingSlipID = packingSlipID;
+    }
+
+    public String getQty() {
+        return qty;
+    }
+
+    public void setQty(String qty) {
+        this.qty = qty;
+    }
+
+    public String getSalesID1() {
+        return salesID1;
+    }
+
+    public void setSalesID1(String salesID1) {
+        this.salesID1 = salesID1;
+    }
+
+    public String getSalesUnit() {
+        return salesUnit;
+    }
+
+    public void setSalesUnit(String salesUnit) {
+        this.salesUnit = salesUnit;
+    }
+
+    public String getTransRecID() {
+        return transRecID;
+    }
+
+    public void setTransRecID(String transRecID) {
+        this.transRecID = transRecID;
+    }
+
+    public String getValueMST() {
+        return valueMST;
+    }
+
+    public void setValueMST(String valueMST) {
+        this.valueMST = valueMST;
+    }
+
+    public salesTable(){
+
+    }
+
+    public salesTable(String dimension, String dimension2_, String dimension3_, String dimension4_, String dimension5_, String address, String city, String countryRegionID, String createdDateTime1, String custAccount, String custGroup, String dataAreaID1, String defaultDimension, String deliveryPostalAddress, String location, String mbp_contactPerson1, String mbp_contactPerson2, String mbp_internalSono, String mbp_phone1, String mbp_phone2, String purchOrderFormNum, String salesGroup, String salesID, String salesName, String salesType, String state, String street, String zipcode, String dataAreaID, String partition, String recID, String dataAreaID2, String partition2, String configID, String deliveryDate, String inventBatchID, String inventDIMID, String inventLocationID, String itemID, String orderAccount, String origSalesID, String packingSlipID, String qty, String salesID1, String salesUnit, String transRecID, String valueMST) {
+        this.dimension = dimension;
+        this.dimension2_ = dimension2_;
+        this.dimension3_ = dimension3_;
+        this.dimension4_ = dimension4_;
+        this.dimension5_ = dimension5_;
+        this.address = address;
+        this.city = city;
+        this.countryRegionID = countryRegionID;
+        this.createdDateTime1 = createdDateTime1;
+        this.custAccount = custAccount;
+        this.custGroup = custGroup;
+        this.dataAreaID1 = dataAreaID1;
+        this.defaultDimension = defaultDimension;
+        this.deliveryPostalAddress = deliveryPostalAddress;
+        this.location = location;
+        this.mbp_contactPerson1 = mbp_contactPerson1;
+        this.mbp_contactPerson2 = mbp_contactPerson2;
+        this.mbp_internalSono = mbp_internalSono;
+        this.mbp_phone1 = mbp_phone1;
+        this.mbp_phone2 = mbp_phone2;
+        this.purchOrderFormNum = purchOrderFormNum;
+        this.salesGroup = salesGroup;
+        this.salesID = salesID;
+        this.salesName = salesName;
+        this.salesType = salesType;
+        this.state = state;
+        this.street = street;
+        this.zipcode = zipcode;
+        this.dataAreaID = dataAreaID;
+        this.partition = partition;
+        this.recID = recID;
+        this.dataAreaID2 = dataAreaID2;
+        this.partition2 = partition2;
+        this.configID = configID;
+        this.deliveryDate = deliveryDate;
+        this.inventBatchID = inventBatchID;
+        this.inventdimid = inventDIMID;
+        this.inventLocationID = inventLocationID;
+        this.itemID = itemID;
+        this.orderAccount = orderAccount;
+        this.origSalesID = origSalesID;
+        this.packingSlipID = packingSlipID;
+        this.qty = qty;
+        this.salesID1 = salesID1;
+        this.salesUnit = salesUnit;
+        this.transRecID = transRecID;
+        this.valueMST = valueMST;
+    }
+}
